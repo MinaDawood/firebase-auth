bbbb85decffbcc3a26f1e0867be86afae2e6e9c4
minadawood <mina.ziden@hotmail.com>
2020-12-30 19:03:45 +0200

    Added Sign up, in, out

---
+++ scripts/auth.js
@@ -0,0 +1,40 @@
+// Sign up
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  // get user info
+  const email = signupForm['signup-email'].value;
+  const password = signupForm['signup-password'].value;
+
+  // sign up the user
+  auth.createUserWithEmailAndPassword(email, password).then(cred => {
+    const modal = document.querySelector('#modal-signup');
+    M.Modal.getInstance(modal).close();
+    signupForm.reset();
+  });
+});
+
+// Logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', e => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+    console.log('User Signed out');
+  });
+});
+
+// Login
+const loginForm = document.querySelector('#login');
+login.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  auth.signInWithEmailAndPassword(email, password).then(cred => {
+    const modal = document.querySelector('#modal-login');
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
+  });
+});
